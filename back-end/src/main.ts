80d57741170734725dc3360be9e38583866719a9
Canario0 <prenero4bc@gmail.com>
2025-04-16 22:20:59 +0200

    feat: integrate Swagger for API documentation
    - Added @nestjs/swagger package to package.json and package-lock.json.
    - Configured Swagger module in main.ts to generate API documentation.
    - Set up Swagger UI at the '/docs' endpoint with basic metadata.

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './apps/rest/app.module';
 import { LOGGER } from './context/shared/domain/logger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(LOGGER));
+
+  const config = new DocumentBuilder()
+    .setTitle('Comparathor API DOCS')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
