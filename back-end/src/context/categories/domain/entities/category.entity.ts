e239981d7bee163018a719f485a63c6e62df2867
Canario0 <prenero4bc@gmail.com>
2025-04-27 21:45:41 +0200

    feat: add version support to CategoryEntity constructor
    - Updated CategoryEntity to include version in the constructor, enhancing entity state management and version tracking.

--- back-end/src/context/categories/domain/entities/category.entity.ts
+++ back-end/src/context/categories/domain/entities/category.entity.ts
@@ -64,6 +64,7 @@ export class CategoryEntity extends AggregateRoot {
             primitives.name,
             primitives.icon,
             primitives.subCategories.map(SubCategory.fromPrimitives),
+            primitives.version,
         );
     }
 
