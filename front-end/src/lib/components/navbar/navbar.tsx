e4c237a9c68b7f930408e822aca3d6c63be7c760
Canario0 <prenero4bc@gmail.com>
2025-05-10 21:40:50 +0200

    feat: enhance NavBar component with Outlet for nested routing
    - Updated NavBar component to include Outlet for rendering nested routes.
    - Maintained existing navigation structure and user action avatar.

--- front-end/src/lib/components/navbar/navbar.tsx
+++ front-end/src/lib/components/navbar/navbar.tsx
@@ -1,24 +1,27 @@
 import { Avatar, ButtonBase } from "@mui/material";
 import styles from "./navbar.module.css";
 import { Person } from "@mui/icons-material";
-import { Link } from "react-router";
+import { Link, Outlet } from "react-router";
 
 function NavBar() {
   return (
-    <nav
-      className={styles.navbar}
-      role="navigation"
-      aria-label="Main Navigation"
-    >
-      <div className={styles.navbar__brand}>
-        <Link to="/">Comparathor</Link>
-      </div>
-      <ButtonBase component="div" tabIndex={-1} aria-label="User Actions">
-        <Avatar>
-          <Person />
-        </Avatar>
-      </ButtonBase>
-    </nav>
+    <>
+      <nav
+        className={styles.navbar}
+        role="navigation"
+        aria-label="Main Navigation"
+      >
+        <div className={styles.navbar__brand}>
+          <Link to="/">Comparathor</Link>
+        </div>
+        <ButtonBase component="div" tabIndex={-1} aria-label="User Actions">
+          <Avatar>
+            <Person />
+          </Avatar>
+        </ButtonBase>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
