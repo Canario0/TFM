235e68b36f7b5bd50741fe9a9583a506d7b1f942
Canario0 <prenero4bc@gmail.com>
2025-06-09 21:53:55 +0200

    feat: implement product and comparison entities with API integration
    - Added Product, ProductSubCategory, and Comparison classes to represent product and comparison data structures.
    - Introduced ProductsService for fetching product details by ID.
    - Enhanced ApiFacade with a method to retrieve products by their ID.
    - Created ComparisonViewModel to manage product comparisons and handle asynchronous loading of product data.

---
+++ android/app/src/main/java/com/example/comparathor/entities/Comparison.java
@@ -0,0 +1,59 @@
+package com.example.comparathor.entities;
+
+import java.util.List;
+
+public class Comparison {
+    private String id;
+    private String ownerId;
+    private String name;
+    private String description;
+    private List<Product> products;
+
+    public Comparison(String id, String ownerId, String name, String description, List<Product> products) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.name = name;
+        this.description = description;
+        this.products = products;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
